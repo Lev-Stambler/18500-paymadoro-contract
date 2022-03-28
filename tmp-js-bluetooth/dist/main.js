1ef1506e5c66cf964cc0ec200d42adb36d02ec46
Lev Stambler <levstamb@gmail.com>
2022-03-28 11:56:26 -0400

    prog on bluetooth

---
+++ tmp-js-bluetooth/dist/main.js
@@ -0,0 +1,90 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// The following code is inspired by https://create.arduino.cc/projecthub/leevinentwilson/bluetooth-node-and-arduino-de822e
+const BTSerialPort = __importStar(require("bluetooth-serial-port"));
+const btSerial = new BTSerialPort.BluetoothSerialPort();
+const errFunction = (err) => {
+    if (err) {
+        console.log("Error", err);
+    }
+};
+const connect = (dataCb) => {
+    return new Promise((res, rej) => {
+        // Once BtSerial.inquire finds a device it will call this code
+        // BtSerial.inquire will find all devices currently connected with your computer
+        btSerial.on("found", (address, name) => __awaiter(void 0, void 0, void 0, function* () {
+            // If a device is found and the name contains 'HC' we will continue
+            // This is so that it doesn't try to send data to all your other connected BT devices
+            if (name.toLowerCase().includes("hc")) {
+                console.log("Found BT module with name", name, "and address", address);
+                btSerial.findSerialPortChannel(address, (channel) => {
+                    console.log("Found channel:", channel);
+                    // Finds then serial port channel and then connects to it
+                    btSerial.connect(address, channel, () => {
+                        // Now the magic begins, bTSerial.on('data', callbackFunc) listens to the bluetooth device.
+                        // If any data is received from it the call back function is used
+                        btSerial.on("data", (b) => dataCb(Buffer.from(b).toString()));
+                        res(btSerial);
+                    }, errFunction);
+                }, 
+                //@ts-ignore
+                errFunction);
+            }
+            else {
+                console.log("Not connecting to: ", name);
+            }
+        }));
+        btSerial.inquire();
+    });
+};
+const callBackData = (data) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("received", data);
+});
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("AAA");
+        const btConn = yield connect(callBackData);
+        console.log("Connected");
+        btConn.write(Buffer.from("From Node With Love\n"), errFunction);
+    });
+}
+main();
+// Are you not entertained?
+/*
+  For this to work you will have to connect to the Bluetooth device on your computer in the normal way
+  I.e via Bluetooth settings: Default password is usually 0000 or 1234
+*/
+// Starts looking for Bluetooth devices and calls the function btSerial.on('found'
+//# sourceMappingURL=main.js.map
---
+++ tmp-js-bluetooth/src/main.ts
@@ -0,0 +1,69 @@
+// The following code is inspired by https://create.arduino.cc/projecthub/leevinentwilson/bluetooth-node-and-arduino-de822e
+import * as BTSerialPort from "bluetooth-serial-port";
+const btSerial = new BTSerialPort.BluetoothSerialPort();
+
+const errFunction = (err) => {
+  if (err) {
+    console.log("Error", err);
+  }
+};
+
+const connect = (
+  dataCb: (string) => Promise<void>
+): Promise<BTSerialPort.BluetoothSerialPort> => {
+  return new Promise((res, rej) => {
+    // Once BtSerial.inquire finds a device it will call this code
+    // BtSerial.inquire will find all devices currently connected with your computer
+    btSerial.on("found", async (address, name) => {
+      // If a device is found and the name contains 'HC' we will continue
+      // This is so that it doesn't try to send data to all your other connected BT devices
+      if (name.toLowerCase().includes("hc")) {
+        console.log("Found BT module with name", name, "and address", address);
+        btSerial.findSerialPortChannel(
+          address,
+          (channel) => {
+            console.log("Found channel:", channel);
+            // Finds then serial port channel and then connects to it
+            btSerial.connect(
+              address,
+              channel,
+              () => {
+                // Now the magic begins, bTSerial.on('data', callbackFunc) listens to the bluetooth device.
+                // If any data is received from it the call back function is used
+                btSerial.on("data", (b) => dataCb(Buffer.from(b).toString()));
+                res(btSerial);
+              },
+              errFunction
+            );
+          },
+          //@ts-ignore
+          errFunction
+        );
+      } else {
+        console.log("Not connecting to: ", name);
+      }
+    });
+    btSerial.inquire();
+  });
+};
+
+const callBackData = async (data: string) => {
+  console.log("received", data);
+};
+
+async function main() {
+	 console.log("AAA")
+  const btConn = await connect(callBackData);
+  console.log("Connected");
+  btConn.write(Buffer.from("From Node With Love\n"), errFunction);
+}
+
+main();
+
+// Are you not entertained?
+
+/*
+  For this to work you will have to connect to the Bluetooth device on your computer in the normal way
+  I.e via Bluetooth settings: Default password is usually 0000 or 1234
+*/
+// Starts looking for Bluetooth devices and calls the function btSerial.on('found'
